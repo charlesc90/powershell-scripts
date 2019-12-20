00b45d4e09e998144b1bbbc90075d803669b6e58
Charles Cox <charles.h.cox@gmail.com>
2019-12-20 15:31:04 -0600

    Update Group-Ping.ps1

--- Group-Ping.ps1
+++ Group-Ping.ps1
@@ -3,7 +3,7 @@ foreach ($Server in $ServerName) {
         if (test-Connection -ComputerName $Server -Count 2 -Quiet ) {
             "$Server is Pinging "
         } 
-		else {
+	else {
             "$Server not pinging"
-		}
+	}
 } 
