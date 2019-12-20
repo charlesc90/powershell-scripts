0800403d43c0da49b002ce8bdd3504ac12c99c16
Charles Cox <charles.h.cox@gmail.com>
2019-12-20 15:29:51 -0600

    Update Get-RemoteComputerTime.ps1

--- Get-RemoteComputerTime.ps1
+++ Get-RemoteComputerTime.ps1
@@ -2,5 +2,5 @@ Write-Host "This script will give you the time on a remote computer."
 $computer = Read-Host -Prompt "Please enter a computer name: " 
 If ($?){
 	Get-WmiObject -Class win32_localtime -ComputerName $computer
-	}
-	Format-Table
+}
+Format-Table
