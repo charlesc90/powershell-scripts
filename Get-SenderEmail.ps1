e61744d25dc68d12c2ed3862f8f2deb19cb869f8
Charles Cox <charles.h.cox@gmail.com>
2019-12-20 14:18:00 -0600

    Create Get-SenderEmail.ps1

---
+++ Get-SenderEmail.ps1
@@ -0,0 +1,20 @@
+########################################################################################################
+# Script: Get-SenderEmail.ps1
+# Author: Charles Cox
+# Date: 20 December 2019
+# Description: This script displays the unique email addresses of senders in a specified mailbox
+########################################################################################################
+
+Function Get-SenderEmail {
+    
+    # Specify mailbox here
+    $Folder = "Inbox"
+
+    Add-Type -AssemblyName "Microsoft.Office.Interop.Outlook" | Out-Null
+    $outlook = New-Object -ComObject Outlook.Application
+    $namespace = $outlook.GetNameSpace("MAPI")
+    $emailAddress = $namespace.Folders.Item(1).Folders.Item($Folder).Items
+    $emailAddress | Sort-Object SenderEmailAddress -Unique | Format-Table SenderEmailAddress
+
+}
+Get-SenderEmail
