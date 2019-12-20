0095823f0915a17684e474b0350cd546c23d1807
Charles Cox <charles.h.cox@gmail.com>
2019-12-20 14:45:05 -0600

    Create Group-Ping.ps1

---
+++ Group-Ping.ps1
@@ -0,0 +1,9 @@
+$ServerName = Get-Content "c:\Computers.txt"
+foreach ($Server in $ServerName) {
+        if (test-Connection -ComputerName $Server -Count 2 -Quiet ) {
+            "$Server is Pinging "
+        } 
+		else {
+            "$Server not pinging"
+		}
+} 
