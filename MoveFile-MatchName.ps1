26bc58dfe49b4b4bde026dafe4323b4ff2ed6500
Charles Cox <charles.h.cox@gmail.com>
2019-12-26 08:37:49 -0600

    Update MoveFile-MatchName.ps1

--- MoveFile-MatchName.ps1
+++ MoveFile-MatchName.ps1
@@ -1,13 +1,8 @@
 ########################################################################################################
-
 # Script: MoveFile-MatchName.ps1
-
 # Author: Charles Cox
-
 # Date: 26 December 2019
-
 # Description: This script moves files based on matching a filename
-
 ########################################################################################################
 
 # This is the source directory
@@ -17,45 +12,31 @@ $dstRoot = "C:\Users\Charles.Cox\Documents\powershell-scripts-master\powershell-
 
 # Get-ChildItem: Gets the contents of the source directory recursively
 # -File: Get list of files
-# -Force: Allows the cmdlet to get items that cannot otherwise not be accessed by the user, such as hidden or system files. The Force parameter does not override security restrictions
-# -Recurse: Gets the items in the specified locations and in all child items of the locations.
+# -Force: Allows the cmdlet to get items that cannot otherwise not be accessed
 $fileList = Get-ChildItem -Path $srcRoot -File -Force -Recurse
 
 # loop through $fileList
 Foreach ($file in $fileList) {
-
    # try to find matches
    # Powershell conditional operators
    # -Match
    # -Like
    # -Contains
    if($file.BaseName -Match 'test-01') {
-
 		#$matches.values gives just the values that match based on -Match flag
         $fileName = $matches.Values
-
         # Store full path to file in $fileToCheck variable
         $fileToCheck ="$dstDir\$file"
-
         #Check if File Exists and if it does, print error
         if (Test-Path $fileToCheck -Pathtype Leaf) {
-
 			Write-Warning "File $file.Name already exists at $dstDir"
-
         }
-
         #If file does not exist then move the file to the destination directory               
         else {
-
 			Move-Item -Path $($file.FullName) -Destination $dstDir
-
         }
     }
-
    else {
-
         $null
-
    }
-
 }
