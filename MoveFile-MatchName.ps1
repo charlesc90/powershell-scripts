5260aff44b69e1503f0f39699cef593e2a8e5edc
Charles Cox <charles.h.cox@gmail.com>
2020-01-21 08:37:29 -0600

    Update MoveFile-MatchName.ps1

--- MoveFile-MatchName.ps1
+++ MoveFile-MatchName.ps1
@@ -1,42 +1,37 @@
 ########################################################################################################
 # Script: MoveFile-MatchName.ps1
-# Author: Charles Cox
-# Date: 26 December 2019
 # Description: This script moves files based on matching a filename
 ########################################################################################################
 
-# This is the source directory
-$srcRoot = "C:\Users\Charles.Cox\Documents\powershell-scripts-master\powershell-scripts-master\test"
-# This is the destination directory
-$dstRoot = "C:\Users\Charles.Cox\Documents\powershell-scripts-master\powershell-scripts-master\test2"
+Function MoveFile-MatchName {
 
-# Get-ChildItem: Gets the contents of the source directory recursively
-# -File: Get list of files
-# -Force: Allows the cmdlet to get items that cannot otherwise not be accessed
-$fileList = Get-ChildItem -Path $srcRoot -File -Force -Recurse
+	# This is the source directory
+	$sourceDir = "C:\test"
+	# This is the destination directory
+	$destinationDir = "C:\test2"
 
-# loop through $fileList
-Foreach ($file in $fileList) {
-   # try to find matches
-   # Powershell conditional operators
-   # -Match
-   # -Like
-   # -Contains
-   If($file.BaseName -Match 'test-01') {
-		#$matches.values gives just the values that match based on -Match flag
-        $fileName = $matches.Values
-        # Store full path to file in $fileToCheck variable
-        $fileToCheck ="$dstDir\$file"
-        #Check if File Exists and if it does, print error
-        If (Test-Path $fileToCheck -Pathtype Leaf) {
-			Write-Warning "File $file.Name already exists at $dstDir"
-        }
-        #If file does not exist then move the file to the destination directory               
-        Else {
-			Move-Item -Path $($file.FullName) -Destination $dstDir
-        }
-    }
-   Else {
-        $null
-   }
+	# Gets the contents of the source directory recursively, including hidden files, excluding directories
+	$fileList = Get-ChildItem -Path $sourceDir -File -Force -Recurse
+	
+	# loop through $fileList
+	Foreach ($file in $fileList) {
+		If($file.BaseName -Match 'test-01') {
+			#$matches.values gives just the values that match based on -Match flag
+			$fileName = $matches.Values
+			# Store full path to file in $fileToCheck variable
+			$fileToCheck ="$destinationDir\$file"
+			#Check if File Exists and if it does, print error
+			If (Test-Path $fileToCheck -Pathtype Leaf) {
+				Write-Warning "File $file.Name already exists at $destinationDir"
+			}
+			#If file does not exist then move the file to the destination directory               
+			Else {
+				Move-Item -Path $($file.FullName) -Destination $destinationDir
+			}
+		}
+		Else {
+			$null
+		}
+	}
 }
+MoveFile-MatchName
