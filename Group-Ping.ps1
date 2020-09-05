dade9fbb9004e9f9df04aecbd565a208277dd310
Charles Cox <charles.h.cox@gmail.com>
2020-09-05 14:18:22 -0500

    Delete Group-Ping.ps1

--- Group-Ping.ps1
+++
@@ -1,9 +0,0 @@
-$ServerName = Get-Content "c:\Computers.txt"
-foreach ($Server in $ServerName) {
-        if (test-Connection -ComputerName $Server -Count 2 -Quiet ) {
-            "$Server is Pinging "
-        } 
-	else {
-            "$Server not pinging"
-	}
-} 
