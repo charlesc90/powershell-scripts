81b64cbc2df3ac697a0cb16b5256754630f83e38
Charles Cox <charles.h.cox@gmail.com>
2020-09-05 14:18:44 -0500

    Delete Get-DirectoriesWithoutInheritedPermissions.ps1

--- Get-DirectoriesWithoutInheritedPermissions.ps1
+++
@@ -1,2 +0,0 @@
-$filepath = "\\server\share"
-Get-ChildItem $filepath -recurse | Select @{Name='Path';Expression={$_.FullName}},@{Name='InheritedCount';Expression={(Get-Acl $_.FullName | Select -ExpandProperty Access | Where { $_.IsInherited }).Count}} | Where { $_.InheritedCount -eq 0 } | Select Path
